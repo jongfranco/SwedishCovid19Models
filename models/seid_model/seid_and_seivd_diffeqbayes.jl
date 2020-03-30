6691e2ee116ea4be0d28bd795631c550d7a7c3c4
Robert Feldt <robert.feldt@gmail.com>
2020-03-30 21:26:47 +0200

    fixed typos in model

--- models/seid_model/seid_and_seivd_diffeqbayes.jl
+++ models/seid_model/seid_and_seivd_diffeqbayes.jl
@@ -6,8 +6,8 @@ using Plots
 
 # SEIV model from eq 2 in [Getz2018], where N = S+E+I+V
 seiv = @ode_def SEIV_Getz2018 begin 
-    dS = λ[t] + ν*V - (τ(I, N) + μ)*S
-    dE = tau[I, N]*S - (σ + μ)*E
+    dS = λ[t] + ν*V - (τ[I, N] + μ)*S
+    dE = τ[I, N]*S - (σ + μ)*E
     dI = σ*E - (γ + α + μ)*I
     dV = γ*I - (v + μ)*V 
 end λ[t], ν, τ[I, N], μ, σ, γ, α
