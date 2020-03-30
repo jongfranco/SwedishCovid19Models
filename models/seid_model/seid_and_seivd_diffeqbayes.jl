187f2d07892a8a819648772db4038e6bbee5597e
Robert Feldt <robert.feldt@gmail.com>
2020-03-30 21:24:07 +0200

    started on basic SEID model but needs work on parameterized params

---
+++ models/seid_model/seid_and_seivd_diffeqbayes.jl
@@ -0,0 +1,14 @@
+using DiffEqBayes
+using OrdinaryDiffEq, RecursiveArrayTools, Distributions, ParameterizedFunctions, Mamba
+using Plots
+
+# params: 
+
+# SEIV model from eq 2 in [Getz2018], where N = S+E+I+V
+seiv = @ode_def SEIV_Getz2018 begin 
+    dS = λ[t] + ν*V - (τ(I, N) + μ)*S
+    dE = tau[I, N]*S - (σ + μ)*E
+    dI = σ*E - (γ + α + μ)*I
+    dV = γ*I - (v + μ)*V 
+end λ[t], ν, τ[I, N], μ, σ, γ, α
+# but not sure how to encode λ[t] and τ[I, N]
